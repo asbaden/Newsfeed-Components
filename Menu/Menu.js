aa19dc60fdf95fc84a65595c820d7b27f1c336d8
Gus Baden <gusbaden96@gamil.com>
2019-12-04 15:40:11 -0800

    finished mvp

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,6 +33,30 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+function createMenu(items){
+
+  const menuMain = document.createElement('div');
+  const listU = document.createElement('ul');
+  
+  menuMain.classList.add("menu");  
+  
+  menuMain.appendChild(listU);
 
 
+items.forEach(item => {
+  let li = document.createElement("li");
+  let text = document.createTextNode(item);
+  li.appendChild(text);
+  listU.appendChild(li)
+});
 
+document.querySelector(".menu-button").addEventListener("click", () => {
+  menuMain.classList.toggle("menu--open")
+});
+
+return menuMain;
+
+}
+const menu = createMenu(menuItems);
+
+document.querySelector(".header").appendChild(menu);
